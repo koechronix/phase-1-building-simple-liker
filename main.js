ff4c129f39279927e8aabeccb7096dfaf0373065
Amos Koech <koechronix@yahoo.com>
2022-06-17 12:13:19 -0400

    commit tree 8d03b01ef27b9418a320b1687449797227b347fe

--- main.js
+++ main.js
@@ -3,6 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.querySelector('#modal').className = 'hidden'
+
+const likeButtons = document.querySelectorAll('.like-glyph')
+
+function handleLikes() {
+  likeButtons.forEach(function(button){
+    button.addEventListener('click', function(event) {
+      if (event.target.innerHTML == EMPTY_HEART) {
+        mimicServerCall('')
+        .then(function(response){
+
+          event.target.innerHTML = FULL_HEART
+          event.target.classList.add('activated-heart')
+
+        })
+        .catch(function(error){
+          let modal = document.querySelector('#modal')
+          modal.classList.remove('hidden')
+          modal.innerText = 'error'
+
+          setTimeout(() => {
+            modal.classList.add('hidden')
+            
+          }, 3000)
+        })
+      }else{
+        event.target.innerHTML = EMPTY_HEART
+        event.target.classList.remove('activated-heart')
+      }
+    })
+  })
+}
+handleLikes()
 
 
 
